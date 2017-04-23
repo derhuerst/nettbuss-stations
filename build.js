1a76c6696aac3f467d44a4931de60466428a0c31
Jannis R <mail@jannisr.de>
2017-04-23 22:28:04 +0200

    fetch raw data from Nettbuss site

---
+++ build.js
@@ -0,0 +1,65 @@
+'use strict'
+
+const fetch = require('isomorphic-fetch')
+const cheerio = require('cheerio')
+const {parse} = require('acorn')
+const {traverse} = require('estraverse')
+
+
+
+const setterName = 'tbAutoCompleteStops'
+
+const isSetterCall = (node) =>
+	node.type === 'CallExpression'
+	&& node.callee.type === 'MemberExpression'
+	&& node.callee.property.type === 'Identifier'
+	&& node.callee.property.name === setterName
+	&& Array.isArray(node.arguments)
+	&& node.arguments[0].type === 'Literal'
+	&& node.arguments[0].value === 'setData'
+	&& node.arguments[1].type === 'ArrayExpression'
+	&& node.arguments[2].type === 'ArrayExpression'
+	&& node.arguments[3].type === 'ArrayExpression'
+
+const setterCallData = (node) => ({
+	stations: node.arguments[1].elements,
+	countries: node.arguments[2].elements
+	// todo: third arg
+	// countryIDs: node.arguments[3].elements
+})
+
+
+
+const showError = (err) => {
+	console.error(err)
+	process.exit(1)
+}
+
+fetch('https://m.nettbuss.se/m/sok-kop-resa', {
+	redirect: 'follow'
+})
+.then((res) => res.text())
+.then((html) => { // find the right <script> tag
+	const $ = cheerio.load(html)
+	const scripts = Array.from($('script'))
+
+	for (let script of scripts) {
+		for (let js of script.children) {
+			if (js.type === 'text' && js.data.indexOf(setterName) >= 0) return js.data
+		}
+	}
+
+	throw new Error('no matching <script> found')
+})
+.then((js) => { // find the fn call containing the data
+	const ast = parse(js)
+	traverse(ast, {
+		enter: (node, parent) => {
+			if (!isSetterCall(node)) return
+			const {stations, countries} = setterCallData(node)
+
+			process.stdout.write(JSON.stringify(stations) + '\n')
+		}
+	})
+})
+.catch(showError)
